7bea001728e368dabf9fe3c5d08aef046c1a6419
Sahib Yar <sahibyaar@hotmail.com>
2017-10-07 23:14:26 +0500

    R-1.3 added.

---
+++ Data structures and algorithms in C++ Book by Michael T. Goodrich/Chapter 1 - A C++ Primer/Reinforcement/R-1.3.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Sahib Yar on 10/7/17.
+//
+
+/*
+ * Give a C++ definition of a struct called Pair that consists of two members.
+ * The first is an integer called first, and the second is a double called second.
+ */
+
+struct Pair
+{
+    int     first;
+    double  second;
+};
