0105499cc1996dde679de4a6c46da544d2a977d1
Sahib Yar <sahibyaar@hotmail.com>
2019-05-10 15:25:22 +0500

    Update prog1.cpp

--- MultiThreading/prog1.cpp
+++ MultiThreading/prog1.cpp
@@ -18,7 +18,7 @@ int main()
 	double res2;
 	
 	std::thread t1{std::bind(f,some_vec, &res1)};		//f(some_vec, &res1) will be executed in separate thread
-	std::thread t2{F(some_vec, &res2)};					//F(some_vec, &res2)() will be executed in separate thread
+	std::thread t2{F(some_vec, &res2)};			//F(some_vec, &res2)() will be executed in separate thread
 	
 	std::cout << res1 << ' ' << res2 << '\n';
 	
