74903ebc53d017edc88bbab51ef7aadaf5dc9733
sahib.yar <sahib.yar@afiniti.com>
2020-08-15 08:07:40 +0500

    string zig-zag conversion done

--- leetcode/0006-zigzag-conversion.cpp
+++ leetcode/0006-zigzag-conversion.cpp
@@ -1,4 +1,44 @@
 //
-// Created by root on 8/15/20.
+// https://leetcode.com/problems/zigzag-conversion/
 //
 
+#include <string>
+class Solution {
+public:
+    std::string convert(std::string s, int numRows) {
+        if (numRows < 2)
+            return s;
+
+        std::string result[numRows];
+
+        int i = 0;
+        bool increment = true;
+        for (const auto &c : s) {
+            result[i].push_back(c);
+
+            if (increment)
+                ++i;
+            else
+                --i;
+
+            if ((i+1) == numRows)
+                increment = false;
+            if(i == 0)
+                increment = true;
+        }
+
+        std::string outstring = "";
+        for (const auto &str : result) {
+            outstring.append(str);
+        }
+        return outstring;
+    }
+};
+
+#include <iostream>
+
+int main() {
+    Solution s;
+    std::cout << s.convert("PAYPALISHIRING", 3) << std::endl;
+    std::cout << s.convert("PAYPALISHIRING", 4) << std::endl;
+}
